89145a49862b1e49e5ca2fd1c10aef70684435ea
Mr. Hasan <hasanshahriar32@gmail.com>
2023-09-07 13:13:55 +0600

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,9 +26,7 @@ HomePage.getLayout = function getLayout(page) {
 };
 
 export const getStaticProps = async () => {
-  const res = await fetch(
-    "https://level2-nextjs-data-fetch.vercel.app:5000/news"
-  );
+  const res = await fetch("http://localhost:5000/news");
   const data = await res.json();
   console.log(data);
   return {
